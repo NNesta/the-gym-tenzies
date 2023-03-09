514e714198f9737fba4984ba166e87aa6a20552f
Ngabonziza Nestor <ngabonest@gmail.com>
2023-03-09 13:08:31 +0200

    feat: refactoring and removing useeffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import useWindowSize from "react-use/lib/useWindowSize";
 import Confetti from "react-confetti";
 
-function App() {
-  const { width, height } = useWindowSize();
-  const [win, setWin] = useState(false);
+const getRandomArray = (size) =>
+  Array.from({ length: size }, () => ({
+    clicked: false,
+    value: Math.floor(Math.random() * 6) + 1,
+  }));
 
-  const [dice, setDice] = useState(() =>
-    Array.from({ length: 10 }, (x) => ({
-      clicked: false,
-      value: Math.floor(Math.random() * 6) + 1,
-    }))
+const App = () => {
+  const { width, height } = useWindowSize();
+  const [dice, setDice] = useState(() => getRandomArray(10));
+  const win = dice.every(
+    (die) => die.clicked === true && die.value === dice[0].value
   );
   const roll = () => {
     setDice((dice) => {
@@ -27,37 +29,32 @@ function App() {
       return newDice;
     });
   };
-  const handleClick = (die, i) => {
+  const handleClick = (die, index) => {
     setDice((prevDice) => {
       const newDice = [...prevDice];
-      newDice[i].clicked = !die.clicked;
+      newDice[index].clicked = !die.clicked;
       return newDice;
     });
   };
-  useEffect(() => {
-    setWin(
-      dice.every((die) => die.clicked === true && die.value === dice[0].value)
-    );
-  }, [dice]);
   return (
-    <main className=" mx-auto my-12 overflow-x-hidden min-h-[517px] bg-white flex items-center justify-center scale-[1.5]">
+    <main className=" mx-auto my-12 overflow-x-hidden min-h-[517px] bg-white flex items-center justify-center scale-150">
       <div className="w-[360px] min-h-[379px] flex items-center justify-center bg-[#0B2434]">
         {win && <Confetti width={width} height={height} />}
         <div className="max-w-[320px] min-h-[320px] bg-[#F5F5F5] rounded-[10px] flex flex-col justify-center gap-4 items-center">
           <h1 className="text-[25.6px] leading-[29.93px] tracking-[-3%] font-bold text-center">
             Tenzies
           </h1>
-          <p className="text-[13.11px] leading-[15.86px] tracking-[-3%] text-center font-normal">
+          <p className="text-sm leading-4 tracking-[-3%] text-center font-normal">
             Roll until all dice are the same. Click each die to freeze it at its
             current value between rolls.
           </p>
           <div className="grid grid-cols-5 gap-2">
-            {dice.map((die, i) => (
+            {dice.map((die, index) => (
               <button
-                onClick={() => handleClick(die, i)}
-                className={`w-[35.84px] ${
+                onClick={() => handleClick(die, index)}
+                className={`w-9 ${
                   die.clicked ? "bg-[#59E391]" : "bg-white"
-                }  aspect-square text-[20.59px] leading-[24.07px] shadow-lg text-center flex items-center justify-center rounded-[4px]`}
+                }  aspect-square text-xl leading-6 shadow-lg text-center flex items-center justify-center rounded-md`}
               >
                 {die.value}
               </button>
@@ -66,23 +63,16 @@ function App() {
           {!win ? (
             <button
               onClick={roll}
-              className="bg-[#5035FF] text-white text-[16.38px] leading-[19.15px] font-bold h-[35.84px] w-[92.16px] flex items-center justify-center"
+              className="bg-[#5035FF] text-white  leading-5 font-bold h-9 w-24 flex items-center justify-center"
             >
               Roll
             </button>
           ) : (
             <button
               onClick={() => {
-                setWin(0);
-
-                setDice(
-                  Array.from({ length: 10 }, (x) => ({
-                    clicked: false,
-                    value: Math.floor(Math.random() * 6) + 1,
-                  }))
-                );
+                setDice(getRandomArray(10));
               }}
-              className="bg-[#5035FF] text-white text-[16.38px] leading-[19.15px] font-bold h-[35.84px] w-[92.16px] flex items-center justify-center"
+              className="bg-[#5035FF] text-white  leading-5 font-bold h-9 w-24 flex items-center justify-center"
             >
               Play again
             </button>
@@ -91,6 +81,6 @@ function App() {
       </div>
     </main>
   );
-}
+};
 
 export default App;
